9f9a9d920f8201659e7f8db4f10e5b62b76f3b6d
NiPavel <87754563+NiPavel@users.noreply.github.com>
2022-12-08 16:51:34 +0200

    Chnaged the Css

--- TravelApp/App_Start/RouteConfig.cs
+++ TravelApp/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace TravelApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "HomePage",
+                url: "",
+                defaults: new {controller = "Home", action = "HomePage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
