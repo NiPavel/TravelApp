859ddf0b4ee90a98f97c503821a5537dc9056625
NiPavel <87754563+NiPavel@users.noreply.github.com>
2022-12-13 20:04:49 +0200

    Start of BackEnd

---
+++ TravelApp/Models/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelApp.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public float Price { get; set; }
+        public int Seats { get; set; }
+    }
+}
